d875283a26689fcb7b999abadf113fc976b0985c
Byron Seto <byronseto@hotmail.com>
2016-10-04 21:30:55 -0600

    Played around with a little bit of js

---
+++ FirstPlayground/something.js
@@ -0,0 +1,20 @@
+function myFunction() {
+   document.getElementById("demo").innerHTML = "Paragraph changed.";
+} 
+
+function windowAlert() {
+    window.alert("Hello");
+}
+
+function webDetails() {
+    var d = new Date();
+    var n = d.getDate();
+    return n;
+}
+
+function changeThat() {
+
+    document.getElementById("changer").innerHTML = "Press Me Now That I've Changed!";
+}
+
+
