082fb37d2a523e2d7488fc027946c07e352b60be
Byron Seto <byronseto@hotmail.com>
2016-10-05 01:28:34 -0600

    WIP, saving for laptop tomorrow

--- SecondPlaygroundSimpleGame/game_functions.js
+++ SecondPlaygroundSimpleGame/game_functions.js
@@ -1,18 +1,25 @@
 function startGame() {
-    document.write("hellosup");
-    var canvas = document.createElement("canvas");
-    canvas.width=300
-    canvas.height=150
-    canvas.style="border:1px solid #d3d3d3;"
-    document.body.insertBefore(canvas, document.body.childNodes[0]);
     //var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    context.clearRect(0,0, 480, 270);
-    context.rect(20,20,150,100);
-    context.stroke();
-    context.fillStyle = "red";
-    context.fillRect(20,20,30,30);
+    gameWorld.start();
+    var mainPlayer = new gameObject(30, 30, "red", 20, 20);
+    mainPlayer.draw();
+    var element = document.getElementById("header");
+    element.innerHTML = "The Bestest Game";
 }
+
+var gameWorld = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width=300;
+        this.canvas.height=150;
+        this.canvas.style="border:1px solid #d3d3d3;";
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+};
 
 function gameObject(width, height, color, x, y) {
     this.width = width;
@@ -23,4 +30,11 @@ function gameObject(width, height, color, x, y) {
     this.velocity_x = 0;
     this.velocity_y = 0;
 
+    this.draw = function() {
+        var context = gameWorld.context;
+        context.rect(this.x, this.y, this.width, this.height);
+        context.stroke();
+        context.fillStyle = "red";
+        context.fillRect(20,20,30,30);
+    }
 }
