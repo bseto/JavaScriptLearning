0183db637d9fa2416accf4541b750a71a6672db4
Byron Seto <byronseto@hotmail.com>
2016-10-05 19:55:36 -0600

    pretty funny, not intended but not bad

--- SecondPlaygroundSimpleGame/game_functions.js
+++ SecondPlaygroundSimpleGame/game_functions.js
@@ -1,8 +1,10 @@
+var mainPlayer
+
 function startGame() {
     //var canvas = document.getElementById("canvas");
     gameWorld.start();
-    var mainPlayer = new gameObject(30, 30, "red", 20, 20);
-    mainPlayer.draw();
+    mainPlayer = new gameObject(30, 30, "red", 20, 20);
+    //mainPlayer.draw();
     var element = document.getElementById("header");
     element.innerHTML = "The Bestest Game";
 }
@@ -15,9 +17,12 @@ var gameWorld = {
         this.canvas.style="border:1px solid #d3d3d3;";
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 2000);
     },
     clear : function() {
+        console.log("Clearing: width:" + this.canvas.width + "height:" + this.canvas.height);
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
     }
 };
 
@@ -27,8 +32,9 @@ function gameObject(width, height, color, x, y) {
     this.color = color;
     this.x = x;
     this.y = y;
-    this.velocity_x = 0;
-    this.velocity_y = 0;
+    this.velocity_x = 5;
+    this.velocity_y = 0.5;
+    this.gravity = 0.1;
 
     this.draw = function() {
         var context = gameWorld.context;
@@ -37,4 +43,17 @@ function gameObject(width, height, color, x, y) {
         context.fillStyle = "red";
         context.fillRect(20,20,30,30);
     }
+
+    this.update = function() {
+        console.log("x:" + this.x)
+        this.x += this.velocity_x;
+        this.velocity_y += this.gravity;
+        this.y += this.velocity_y;
+    }
+}
+
+function updateGameArea() {
+    gameWorld.clear();
+    mainPlayer.update();
+    mainPlayer.draw();
 }
